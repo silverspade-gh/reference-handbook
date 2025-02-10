22fefe064f111e31dfd3a848e466e23a00c5778c
silverspade-gh <silver@bucketofdata.com>
2025-02-09 22:34:15 -0500

    Create reference.js
    Added .join and .split array functions

---
+++ reference.js
@@ -0,0 +1,15 @@
+/* Reference.js
+** A file that provides tips on JS usage.
+*/
+
+// Join an array into a string using a delimiter
+myArray = [1, 2, 3];
+myDelimiter = '-'
+joinedArray = myArray.join();
+console.log(joinedArray) // Prints '1-2-3'
+
+// Split a string into an array using a delimiter
+myArray = '1-2-3';
+myDelimiter = '-'
+joinedArray = myArray.split('-');
+console.log(joinedArray) // Prints [1, 2, 3]
